94a3ff5ba94058b570c75f5bf828f72fb630f00f
luis_morlaes@uadec.edu.mx <luis_morales@uadec.edu.mx>
2020-05-25 21:27:54 -0500

    Agregado Game App con Flask

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/juegos', methods=['POST'])
-def get_juego():
-    game = request.form["search"]
-    rawg = rawg_juego()
-    game = build_juego(rawg,game)
-    data = game.get_everything()
-    return render_template('juegos.html', data = data)
+@app.route('/juegos')
+def juegos():
+    return render_template('juegos.html')
 
+@app.route('/resultado', methods=['POST'])
+def get_juego():
+    game = request.form['search']
+    rawg = rawg_juego()
+    game = build_juego(rawg, game)
+    data = game.get_everything()
+    return render_template("resultados.html", data= data)
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0")
